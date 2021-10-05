aa9d3a02e90099fec482e719cd1bdbcb81ee35da
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-10-05 17:55:40 +0200

    ✨ Quit Unity editor on exit

--- LD49/Assets/_Project/Scripts/UI/ExitGame.cs
+++ LD49/Assets/_Project/Scripts/UI/ExitGame.cs
@@ -4,6 +4,10 @@ public class ExitGame : MonoBehaviour
 {
     public void Exit()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
